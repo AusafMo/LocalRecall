9bbdf2061cb72adc459f4ecfce55c342d373adf5
AusafMo <ausaf9922@gmail.com>
2024-06-03 00:10:58 +0530

    testing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from modules.dotQuery import dotQuery
 from time import time
 
 s = time()
-res = dotQuery(inputText = "snapshot of a white screen with blue and black text ", topk = 2, 
+res = dotQuery(inputText = "image of a white background", topk = 5, 
                includeDistance = True, includeVector = False, pineCone = True)
 
 print(res, "\n",time()-s)
